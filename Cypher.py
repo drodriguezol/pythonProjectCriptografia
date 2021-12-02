ff167a0839f7651a1d8d827287968f5e48089fdf
judgomezri <judgomezri@unal.edu.co>
2021-12-02 00:26:06 -0500

    interfaz analisis hill e interfaz hill imagenes

--- Cypher.py
+++ Cypher.py
@@ -381,7 +381,48 @@ def vigenereClave(texto):
         return claveFinal
 
 
-
+#HILL
+def texto_en_matriz(texto,m):
+    letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    letrasIndice = dict(zip(letras, range(len(letras))))
+    split_P=[]
+    message_in_numbers = []
+    for letter in texto:
+      message_in_numbers.append(letrasIndice[letter])
+      if(len(texto)%m==0):
+       split_P = [
+       message_in_numbers[i : i + m]
+       for i in range(0, len(message_in_numbers), m) ]
+    return(split_P)
+def CriptoanalisisHill(textocifrado,textodecifrado):
+    letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    indiceLetras = dict(zip(range(len(letras)), letras))
+    claves=[]
+    m=1
+    textoclave=""
+    textocifrado=textocifrado.upper()
+    textodecifrado=textodecifrado.upper()
+    for k in range(int(len(textocifrado)**0.5 - 1)):
+        textoclave=""
+        m=m+1
+        try:
+            a=texto_en_matriz(textocifrado,m)
+            a=a[0:m]
+            b=texto_en_matriz(textodecifrado,m)
+            b=b[0:m]
+            #print(b,"chao ",m)
+            acuadrada=inversaMatriz(a,26)
+            key=acuadrada.dot(b)%26  
+            for i in range(m):
+                for j in range(m):
+                    textoclave+=indiceLetras[key[i][j]]
+            if(hillCifrar(textocifrado,textoclave)==textodecifrado): 
+                tupla=(str(textoclave),int(m))
+                claves.append(tupla)
+        except:
+            errorNotLoud=0
+            #print("hay un error en la longitud del texto a cifrar para m =",m,"")
+    return(claves)
 
 
 
@@ -401,22 +442,96 @@ def vigenereClave(texto):
 
 
 import numpy as np
-import matplotlib.pyplot as plt
 import requests
 import imageio as iio
+from numpy.linalg import inv, det
+from PIL import Image
+
+
+def hillImagenCifrar():
+    img, original_shape = loadImage2(0)
+    slice=computer_slice(img)
+    key = np.random.random_integers(0, 100, (slice, slice))
+    while det(key) == 0:
+        key = np.random.random_integers(0, 100, (slice, slice))
+    reversedKey = np.matrix(key).I.A
+    encoded_image_vector = encode(slice,img[0],key)
+    with open('key.npy', 'wb') as f:
+        np.save(f, reversedKey)
+        np.save(f, np.array([slice]))
+        np.save(f,encoded_image_vector)
+    encoded_image = encoded_image_vector.reshape(original_shape)
+    encoded_image = encoded_image.astype('uint8')
+    im = Image.fromarray(encoded_image)
+    im = im.convert('RGB')
+    im.save("imagenCifrada.jpg")
+
+def hillImagenDescifrar():
+    img,original_shape = loadImage2(1)
+    with open('key.npy', 'rb') as f:
+        reversedKey = np.load(f)
+        slice=np.load(f)
+        vector=np.load(f)
+    slice=slice[0]
+    decoded_image_vector = decode(slice,vector,reversedKey)
+    decoded_image = decoded_image_vector.reshape(original_shape)
+    decoded_image = decoded_image.astype('uint8')
+    im = Image.fromarray(decoded_image)
+    im = im.convert('RGB')
+    im.save("imagen.jpg")
+
+def computer_slice(image):
+    max_slice = 100
+    data_shape = image.shape[1]
+
+    for i in range(max_slice, 0, -1):
+        if data_shape % i == 0:
+            return i
 
 def loadImage(url):
     f = open('imagen.jpg', 'wb')
     f.write(requests.get(url).content)
     f.close()
     image = iio.imread('imagen.jpg')
-    
-    reshape = 1
+    im = Image.fromarray(image)
+    im = im.convert('RGB')
+    im.save("imagen.jpg")
 
+def loadImage2(a):
+    if(a==0):
+        image = iio.imread('imagen.jpg')
+    elif(a==1):
+        image = iio.imread('imagenCifrada.jpg')
+    reshape = 1
     for i in image.shape:
         reshape *= i
-
     return image.reshape((1, reshape)), image.shape
+
+def loadImage3(path):
+    image = iio.imread(path)
+    im = Image.fromarray(image)
+    im = im.convert('RGB')
+    im.save("imagen.jpg")
+
+def encode(slice, data, key):
+        crypted = []
+        for i in range(0, len(data), slice):
+            temp = list(np.dot(key, data[i:i + slice]))
+            crypted.append(temp)
+
+        crypted = (np.array(crypted)).reshape((1, len(data)))
+        return crypted[0]
+
+def decode(slice, data, reversed_key):
+        uncrypted = []
+
+        for i in range(0, len(data), slice):
+            temp = list(np.dot(reversed_key, data[i:i + slice]))
+            uncrypted.append(temp)
+
+        uncrypted = (np.array(uncrypted)).reshape((1, len(data)))
+
+        return uncrypted[0]
 
 
 
