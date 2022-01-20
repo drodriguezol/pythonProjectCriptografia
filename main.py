d930f49d7d1dda5ab45d45a40b6f79f15b9df19a
judgomezri <judgomezri@unal.edu.co>
2022-01-20 10:13:10 -0500

    gamma Pentagonal completo

--- main.py
+++ main.py
@@ -1692,8 +1692,13 @@ class CriptoGamma(ttk.Frame):
 
         def generarGrafo():
             try:
+                axs.clear()
                 xVal=xText.get("1.0","end-1c")
                 yVal=yText.get("1.0","end-1c")
+                aumentarAlph(int(xVal),int(yVal))
+                iniciarAlph()
+                annotatePlot()
+                canvas.draw()
                 modo=0
                 if(self.combo.get()=="Grafo 2"):
                     modo=1
@@ -1780,11 +1785,11 @@ class CriptoGamma(ttk.Frame):
 
         
 
-        def create_graph(x, y):
-            n = 15
+        def graficarGrafo(x, y):
+            n=5
             vect=None
             if(self.combo.get()=='Grafo 2'):
-                vect=graph2(x, y, n)
+                vect=grafo2(x, y, n)
             else:
                 edges = set()
                 curr = (x, y)
@@ -1826,14 +1831,14 @@ class CriptoGamma(ttk.Frame):
             return vect
 
 
-        def graph2(x, y, n):
+        def grafo2(x, y, n):
             if((x+y)!=0):
                 x=0
                 y=0
             if n == 0:
                 return {((x, y), (x + 1, y))}
             else:
-                last = graph2(x, y, n - 1)
+                last = grafo2(x, y, n - 1)
                 borde = set([segmento for segmento in last if segmento[1][0] == n])
                 minimo = min([i[1][1] for i in borde])
                 temp = set()
@@ -1853,7 +1858,7 @@ class CriptoGamma(ttk.Frame):
         a=fig2.add_subplot(111)
 
         def crearGrafo(x,y,mode):
-            vect= create_graph(x, y)
+            vect= graficarGrafo(x, y)
             #plt.axis('equal')
             a.set_xlim([x, x + 15])
             a.set_ylim([y, y + 20])
@@ -1890,60 +1895,115 @@ class CriptoGamma(ttk.Frame):
         toolbar.update()
 
 
+        def obtenerVector(x,y):
+            n=5
+            if self.combo.get() == 'Grafo 1':
+                edges = set()
+                curr = (x, y)
+                for i in range(n + 1):
+                    next = (curr[0] + 1, curr[1] + i)
+                    segment = (curr, next)
+                    curr = next
+                    edges.add(segment)
+                dirA = edges
+                edges = set()
+                for segment in dirA:
+                    last = segment[1]
+                    edgetm = set()
+                    curr = (*last, n)
+                    for i in range(n + 1):
+                        next = (curr[0] + 1, curr[1] + i)
+                        segment = (curr, next)
+                        curr = next
+                        edgetm.add(segment)
+                    temp = edgetm
+                    edges = edges.union(temp)
+                dirB = edges
+                edges = set()
+                for segment in dirB:
+                    last = segment[1]
+                    pendiente = segment[1][1] - segment[0][1]
+                    edgetm = set()
+                    curr = (*last, pendiente)
+                    for i in range(n + 1):
+                        next = (curr[0] + 1, curr[1] + i)
+                        segment = (curr, next)
+                        curr = next
+                        edgetm.add(segment)
+                    temp = edgetm
+                    edges = edges.union(temp)
+                dirC = edges
+                total = set()
+                vect = ((total.union(dirA)).union(dirB)).union(dirC)
+            else:
+                vect = grafo2(x,y,n)
+            return vect
+
+
+        def contador(x,y,path):            
+            count = 0
+            for segment in path:
+                if (x,y) == segment[1]:
+                    count += 1
+            return count
+
+        def aumentarAlph(x,y):
+            pathh = obtenerVector(x, y)
+            for i in range(10):
+                for j in range(20):
+                    n = contador(i,j, pathh) # n es lo que se desplaza
+                    matrixAlph[i][j] = (matrixAlph[i][j]+n)%26
 
 
         #funciones para los botones
         def cifrar():
-            text=texto.get("1.0","end-1c").replace(" ","").replace("\n","")
-            password=clave.get("1.0","end-1c").replace(" ","")
-            boolPass=True
+            text=texto.get("1.0","end-1c").replace(" ","").replace("\n","").lower()
 
             if (not text.isalpha()) and not text.split(" ")==['']:
                     messagebox.showinfo("Advertencia","Sólo se admiten letras en el texto.")
                     main_window.deiconify()
+            try:
+                cifrado=""
+                text=list(text)
+                k=len(text)
+                while k > 0:
+                    for i in range(10):
+                        for j in range(20):
+                            print(i,j, k)
+                            if(chr(matrixAlph[i][j]+97)==text[len(text)-k]):
+                                cifrado+=('('+str(i)+','+str(j)+');')
+                                print(cifrado)
+                                k-=1
+                            if(k==0):
+                                break
+                        if(k==0):
+                            break
+                
+                resultado.configure(state='normal')
+                resultado.delete("1.0", END)
+                resultado.insert(INSERT, cifrado[:-1])
+                resultado.configure(state='disabled')
 
-            elif(self.combo.get())=='Desplazamiento': 
-                try:
-                    password=(int(password))
-                    if (password<0):
-                        boolPass=False
-                except:
-                    boolPass=False
-                if(not boolPass):
-                    messagebox.showinfo("Advertencia","Sólo se admiten números naturales como clave.")
-                    main_window.deiconify()
-                else:
-                    resultado.configure(state='normal')
-                    resultado.delete("1.0", END)
-                    resultado.insert(INSERT, desplazamientoCifrar(text,password))
-                    resultado.configure(state='disabled')
+            except:
+                messagebox.showinfo("Advertencia","Hay un error con el texto.")
+                main_window.deiconify()
                            
 
         def descifrar():
-            text=texto.get("1.0","end-1c").replace(" ","").replace("\n","")
-            password=clave.get("1.0","end-1c")
-            boolPass=True
-            if (not text.isalpha()) and not text.split(" ")==['']:
-                    messagebox.showinfo("Advertencia", "Sólo se admiten letras en el texto.")
-                    main_window.deiconify()
+            try:
+                text=texto.get("1.0","end-1c").split(";")
+                descifrado = ""
+                for coord in text:
+                    coords = coord.split(',')
+                    descifrado+=chr(matrixAlph[int(coords[0][1:])][int(coords[1][:-1])]+97)
 
-            if (self.combo.get())=='Desplazamiento':
-                try:
-                    password=(int(password))
-                    if (password<0):
-                        boolPass=False
-                except:
-                    boolPass=False
-                
-                if(not boolPass):
-                    messagebox.showinfo("Advertencia","Sólo se admiten números naturales como clave.")
-                    main_window.deiconify()
-                else:    
-                    resultado.configure(state='normal')
-                    resultado.delete("1.0", END)
-                    resultado.insert(INSERT, desplazamientoDescifrar(text,password))
-                    resultado.configure(state='disabled')
-
+                resultado.configure(state='normal')
+                resultado.delete("1.0", END)
+                resultado.insert(INSERT, descifrado)
+                resultado.configure(state='disabled')
+            except:
+                messagebox.showinfo("Advertencia","No es posible descifrar el texto. Compruebe que el formato es correcto.")
+                main_window.deiconify()
             
         def copiar_al_portapapeles():
             self.clipboard_clear()
